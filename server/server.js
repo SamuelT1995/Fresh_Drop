335465a34e803826781d017ebdfa525a6027a5ac
SamuelT1995 <samueltenagne@gmail.com>
2025-07-22 20:53:44 +0300

    backend

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import connectDB from "./configs/db.js";
+import "dotenv/config";
+import userRouter from "./routes/userRoute.js";
+import sellerRouter from "./routes/sellerRoute.js";
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+await connectDB();
+
+//allow multiple origins
+const allowedOrigins = ["http://localhost:5173"];
+
+//middleware configuration
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+app.get("/", (req, res) => res.send("API IS WORKING"));
+app.use("/api/user", userRouter);
+app.use("/api/seller", sellerRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
