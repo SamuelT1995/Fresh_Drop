a1edb459c383a60f5130df920999782a6f9e033a
samuelT1995 <samueltenagne1995@gmail.com>
2026-08-22 13:21:53 +0300

    Replace cors with custom dynamic origin middleware for credentials support

--- server/index.js
+++ server/index.js
@@ -21,12 +21,20 @@ connectCloudinary(); // Connect to Cloudinary
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
 app.use(cookieParser());
-app.use(cors({
-    origin: true,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
-}));
+// Custom CORS middleware to guarantee credential support without wildcard '*'
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (origin) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.get('/', (req, res) => {
   res.send('API is running...');
